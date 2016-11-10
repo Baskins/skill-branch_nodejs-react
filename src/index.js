ab6b8c851f20f7143e32c99fba63172b954a079c
Baskin <baskin.sasha@gmail.com>
2016-11-10 14:02:22 +0300

    Решение от коллеги https://bitbucket.org/Grey2k/skb-2.git

--- src/index.js
+++ src/index.js
@@ -1,72 +1,62 @@
 import express from 'express';
-import cors from 'cors';
+import _ from 'lodash';
+import XRegExp from 'xregexp';
 
 const app = express();
-app.use(cors());
+const allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://account.skill-branch.ru');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
+
+app.use(allowCrossDomain);
+
 app.get('/', (req, res) => {
   res.redirect('/task-2B');
 });
 
-app.get('/task-2B', (req, res) => {
-  // Перевести полное `Имя Отчество Фамилия` в `Фамилия И. О.`
-  const fullName = req.query.fullname;
-  const arr = fullName.split(' ');
-
-  if (arr.length > 3 || fullName === '' || /[0-9_,|'"]|\/|\*/gi.test(fullName)) {
-    var result = 'Invalid fullname';
-
-  } else if (arr.length > 2) {
-
-    var name = arr[0];
-    if (typeof name === 'undefined') {
-      var name = '';
-    } else {
-      var name = name.charAt(0) + '.';
+// решение от коллеги https://bitbucket.org/Grey2k/skb-2.git
+app.get('/task-2B', function (req, res) {
+    var ERROR = 'Invalid fullname';
+    if (typeof req.query.fullname === 'undefined') {
+      console.log('Undefined param', XRegExp);
+      res.send(ERROR);
+      return;
     }
 
-    var middlename = arr[1];
-    if (typeof middlename === 'undefined') {
-      var middlename = '';
-    } else {
-      var middlename = middlename.charAt(0) + '.';
+    var fullname = _.trim(req.query.fullname);
+    var pattern = XRegExp("^\\s*([\\p{L}']+)\\s*([\\p{L}']+)?\\s*([\\p{L}']+)?\\s*$", 'i');
+    if (!pattern.test(fullname)) {
+      console.log('Bad format = ', fullname);
+      res.send(ERROR);
+      return;
     }
 
-    var surname = arr[2];
-    if (typeof surname === 'undefined') {
-      var surname = '';
+    var matches = _.filter(fullname.match(pattern).slice(1), function (match) {
+        return typeof match !== 'undefined';
+      });
+
+    var fio = Array();
+    switch (matches.length) {
+      case 1:
+        fio.push(matches[0].charAt(0).toUpperCase() + matches[0].slice(1).toLowerCase());
+        break;
+      case 2:
+        fio.push(matches[1].charAt(0).toUpperCase() + matches[1].slice(1).toLowerCase());
+        fio.push(matches[0].charAt(0).toUpperCase() + '.');
+        break;
+      case 3:
+        fio.push(matches[2].charAt(0).toUpperCase() + matches[2].slice(1).toLowerCase());
+        fio.push(matches[0].charAt(0).toUpperCase() + '.');
+        fio.push(matches[1].charAt(0).toUpperCase() + '.');
+        break;
+      default:
+        res.send(ERROR);
+        return;
     }
-
-    var result = `${surname} ${name} ${middlename}`;
-
-  } else if (arr.length > 1) {
-
-    var name = arr[0];
-    if (typeof name === 'undefined') {
-      var name = '';
-    } else {
-      var name = name.charAt(0) + '.';
-    }
-
-    var surname = arr[1];
-    if (typeof surname === 'undefined') {
-      var surname = '';
-    }
-
-    var result = `${surname} ${name}`;
-
-  } else {
-
-    var surname = arr[0];
-    if (typeof surname === 'undefined') {
-      var surname = '';
-    }
-
-    var result = `${surname}`;
-  }
-
-  res.send(result);
-
-});
+    res.send(fio.join(' '));
+  });
 
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
